bf4a2d3eed4314d3d8927ddb5c6b434e58b2439a
Daria Onishchenko <daria.onishchenko.57@gmail.com>
2023-11-03 17:20:24 +0200

    added  local host db on mysql (nonsecure)

--- register.js
+++ register.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const { error } = require('console')
 const bodyParser = require('body-parser')
-const app = express();
+const mysql = require('mysql');
+//const { errorMonitor } = require('events');
 
-app.listen(3000, ()=>{
-    console.log("Running on port 3000")
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'users'
 })
+
+db.connect(err =>{
+    if(err){
+        throw err
+    }
+    console.log('MySQL Connected')
+})
+
+const app = express();
 
 //for css
 app.use(express.static(__dirname))
@@ -13,14 +27,43 @@ app.use(express.static(__dirname))
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/INDEX.html')
+    res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/adduser', (req, res) => {
-    var username = req.body.userlogin;
-    var useremail = req.body.useremail;
+    if(!db){
+        db.query('CREATE DATABASE users', err => {
+            if(err){
+                throw err
+            }
+            console.log('Database created')
+        }) 
+        
+        let sql = 'CREATE TABLE users (id int AUTO_INCREMENT, name VARCHAR (255), password VARCHAR (255), email VARCHAR (255), PRIMARY KEY(id))'
+            db.query(sql, err => {
+                if(err){
+                    throw err
+                }
+                console.log('users table created')
+            })
+    }
+    else{
+        var username = req.body.userlogin;
+        var useremail = req.body.useremail;
+        var userpassword = req.body.userpassword;
 
-    //res.send("Hello " + username + ", thank you for subcribing. Your email is " + useremail)
-    res.sendFile(__dirname + "/account-page.html")
+        let post = {name: username , password: userpassword, email: useremail}
+        let sql = 'INSERT INTO users SET ?'
+        let query = db.query(sql,post, err => {
+            if(err){
+                throw err
+            }
+            res.sendFile(__dirname + "/account-page.html")
+        })
+    }
+    
 })
 
+app.listen(3000, ()=>{
+    console.log("Running on port 3000")
+})
