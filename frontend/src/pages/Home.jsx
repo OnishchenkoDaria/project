8378aaedff4f251e52dbcb953d3c96c2f91fc97f
Diana Klymova <dianaklimova0904@gmail.com>
2023-12-08 01:08:59 +0200

    added homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,14 @@ import PostCreate from '../components/PostCreate.jsx'
 import PostDisplayMain from '../components/PostDisplayMain.jsx'
 import postService from '../services/posts.js'
 import registerService from '../services/registerForm.js';
+import Header from '../components/Header';
+import Navbar from "../components/Navbar";
+import PhotoCarousel from "../components/PhotoCarousel";
+import PhotographerExperience from "../components/PhotographerExperience";
+import BlogList from "../components/BlogList";
+import Comments from "../components/Comments";
+import OrderButton from "../components/OrderButton";
+import Footer from "../components/Footer";
 
 const Home = () => {
   const [isAdmin, setIsAdmin] = useState(false)
@@ -22,7 +30,7 @@ const Home = () => {
 
   return (
 
-    <div className="text">
+    /*<div className="text">
       <button><Link to={PathConstants.PAYMENT}>BUY PHOTOSHOOT</Link></button>
       <PostDisplayMain posts={posts}/>
       <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
@@ -30,7 +38,19 @@ const Home = () => {
       <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
       <PostCreate isAdmin={isAdmin}/>
-    </div>
+    </div>*/
+
+    <div>
+            
+            <Navbar />
+            <PhotoCarousel />
+            <PhotographerExperience />
+            <BlogList />
+            <Comments />
+            <OrderButton />
+            
+
+        </div>
   );
 }
 
